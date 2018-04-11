e6b66a0907245cbbfd144e0d64531be0ec665c74
Johannes Frey <me@johannes-frey.de>
2018-04-11 22:10:27 +0200

    Add binaries under $HOME/opt to PATH

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,13 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
-export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+# Add binaries under $HOME/opt to PATH
+
+OPT_BINS_COUNT=($HOME/opt/*/bin(N))
+
+if [ ! $#OPT_BINS_COUNT -eq 0 ]; then
+    OPT_BINS=($HOME/opt/*/bin(:a))
+    OPT_BINS_PATH="${OPT_BINS:gs/ /:/}"
+    PATH="./bin:${OPT_BINS_PATH}:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+else
+    PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+fi
+
+export PATH
