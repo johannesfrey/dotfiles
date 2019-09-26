54d5e80302386a3f416a829e1248672492209695
Johannes Frey <me@johannes-frey.de>
2019-09-26 21:43:56 +0200

    Use gnu-sed

--- system/_path.zsh
+++ system/_path.zsh
@@ -5,9 +5,9 @@ OPT_BINS_COUNT=($HOME/opt/*/bin(N))
 if [ ! $#OPT_BINS_COUNT -eq 0 ]; then
     OPT_BINS=($HOME/opt/*/bin(:a))
     OPT_BINS_PATH="${OPT_BINS:gs/ /:/}"
-    PATH="${OPT_BINS_PATH}:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+    PATH="${OPT_BINS_PATH}:/usr/local/opt/gnu-sed/libexec/gnubin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 else
-    PATH="/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+    PATH="/usr/local/opt/gnu-sed/libexec/gnubin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 fi
 
 export PATH
